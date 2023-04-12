fb18999aad90130e57eea56a3935cabac6b8f003
Dmitry <74065724+GribanovDS@users.noreply.github.com>
2023-04-12 14:49:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import anderson_ksamp
+# Используем критерий Андерсон-Дарлинга
 
+chat_id = 1219503064 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+# Нулевая гипотеза - выборки однородны, если p_value > alpha, то принимаем нулевую гипотезу
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04
+    p = anderson_ksamp([x, y]).significance_level
+    return (p < alpha)
